25f05bd06821f75f2e11c209d0df847777f2d4ec
adnan2508 <adnandiu2800@gmail.com>
2026-05-30 01:37:16 +0600

    map component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+import Map from "@/components/Map";
 
 
 export default function Home() {
   return (
     <div >
-      <main >
-        <h1 className="text-4xl text-red-500">Welcome to Geo Location App</h1>
+      <main className="min-h-screen bg-gray-100 p-6">
+        <div className="max-w-4xl mx-auto">
+          <h1 className="text-4xl font-bold mb-6">
+            Geolocation App
+          </h1>
+
+          <button className="bg-black text-white px-6 py-3 rounded-lg">
+            Get My Location
+          </button>
+
+          <Map />
+        </div>
+
       </main>
     </div>
   );
